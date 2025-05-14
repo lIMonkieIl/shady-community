d577aa1159ce79ad0e65b5ca1d2c9e0f4ec7e6f6
lIMonkieIl <a_brine@yahoo.com>
2025-05-14 10:18:47 +0100

    Comment out ToastListener in layout.tsx to improve code clarity and maintainability.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
-import { ToastListener } from "../components/layout/toastListener";
+// import { ToastListener } from "../components/layout/toastListener";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -61,7 +61,7 @@ export default function RootLayout({
 					{/* <footer className="p-4">(footer)</footer> */}
 				</div>
 				<Toaster position="top-right" />
-				<ToastListener />
+				{/* <ToastListener /> */}
 			</body>
 		</html>
 	);
